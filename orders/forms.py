8a734fa596f5f4e6e61390d8a4c59b29aa00bb04
smarak1996 <smarakprd5@gmail.com>
2022-03-25 10:19:21 +0530

    Paypal Integration

---
+++ orders/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Order
+
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+
+        widgets = {
+            'first_name' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'last_name' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'email' : forms.EmailInput(attrs = {'class' : 'form-control'}),
+            'address' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'postal_code' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'city' : forms.TextInput(attrs = {'class' : 'form-control'}),
+
+        }
